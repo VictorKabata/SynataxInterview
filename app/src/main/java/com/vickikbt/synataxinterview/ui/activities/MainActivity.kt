935cc9be289c6ece2c509243fb76be2e0e08d084
Victor Kabata Ndung'u <39780120+VictorKabata@users.noreply.github.com>
2020-11-13 10:45:57 +0300

    Added project structure

--- app/src/main/java/com/vickikbt/synataxinterview/ui/activities/MainActivity.kt
+++ app/src/main/java/com/vickikbt/synataxinterview/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.vickikbt.synataxinterview
+package com.vickikbt.synataxinterview.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vickikbt.synataxinterview.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
